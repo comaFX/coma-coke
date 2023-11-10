603f947296e3142f9b997252391f13e0485c349d
coma <146189420+comaFX@users.noreply.github.com>
2023-11-10 05:22:00 -0600

    Update target.lua

--- client/target.lua
+++ client/target.lua
@@ -26,13 +26,13 @@ end)
 
 CreateThread(function()
 
-	exports["qb-target"]:AddBoxZone("EnterTheCokeRoom", vector3(115.4, -1685.58, 32.85), 0.1, 1.5, {
+exports["qb-target"]:AddBoxZone("EnterTheCokeRoom", vector3(115.4, -1685.58, 32.85), 0.1, 1.5, {
         name = "EnterTheCokeRoom",
         heading = 230,
         debugPoly = false,
 		minZ = 30.85,
 		maxZ = 34.85,
-    }, {
+}, {
         options = {
             {
                 type = "client",
@@ -62,14 +62,14 @@ CreateThread(function()
     distance = 2.0
     })
 
-	exports["qb-target"]:AddBoxZone("cokeleafproc", vector3(Config.Zones.CokeProcessing.coords["x"], Config.Zones.CokeProcessing.coords["y"], Config.Zones.CokeProcessing.coords["z"]), Config.Zones.CokeProcessing.width, Config.Zones.CokeProcessing.length, {
+exports["qb-target"]:AddBoxZone("cokeleafproc", vector3(Config.Zones.CokeProcessing.coords["x"], Config.Zones.CokeProcessing.coords["y"], Config.Zones.CokeProcessing.coords["z"]), Config.Zones.CokeProcessing.width, Config.Zones.CokeProcessing.length, {
         name = "cokeleafproc",
         --heading = 88.35,
 		heading = Config.Zones.CokeProcessing.heading,
         debugPoly = false,
 		minZ = Config.Zones.CokeProcessing.minZ,
 		maxZ = Config.Zones.CokeProcessing.maxZ,
-    }, {
+}, {
         options = {
             {
                 type = "client",
@@ -81,7 +81,7 @@ CreateThread(function()
     distance = 1.5
     })
 
-	exports["qb-target"]:AddBoxZone("cokepowdercut", vector3(Config.Zones.CokePowder.coords["x"], Config.Zones.CokePowder.coords["y"], Config.Zones.CokePowder.coords["z"]), Config.Zones.CokePowder.width, Config.Zones.CokePowder.length, {
+exports["qb-target"]:AddBoxZone("cokepowdercut", vector3(Config.Zones.CokePowder.coords["x"], Config.Zones.CokePowder.coords["y"], Config.Zones.CokePowder.coords["z"]), Config.Zones.CokePowder.width, Config.Zones.CokePowder.length, {
         name = "cokepowdercut",
 		--heading = 180.22,
 		heading = Config.Zones.CokePowder.heading,
@@ -90,7 +90,7 @@ CreateThread(function()
         --maxZ = -36.99,
 		minZ = Config.Zones.CokePowder.minZ,
 		maxZ = Config.Zones.CokePowder.maxZ,
-    }, {
+}, {
         options = {
             {
                 type = "client",
@@ -102,13 +102,13 @@ CreateThread(function()
     distance = 1.5
     })
 
-    exports["qb-target"]:AddBoxZone("cokebricked", vector3(Config.Zones.CokeBrick.coords["x"], Config.Zones.CokeBrick.coords["y"], Config.Zones.CokeBrick.coords["z"]), Config.Zones.CokeBrick.width, Config.Zones.CokeBrick.length, {
+exports["qb-target"]:AddBoxZone("cokebricked", vector3(Config.Zones.CokeBrick.coords["x"], Config.Zones.CokeBrick.coords["y"], Config.Zones.CokeBrick.coords["z"]), Config.Zones.CokeBrick.width, Config.Zones.CokeBrick.length, {
         name = "cokebricked",
         heading = Config.Zones.CokeBrick.heading,
         debugPoly = false,
         minZ = Config.Zones.CokeBrick.minZ,
 		maxZ = Config.Zones.CokeBrick.maxZ,
-    }, {
+}, {
         options = {
             {
                 type = "client",
@@ -120,13 +120,13 @@ CreateThread(function()
     distance = 1.5
     })
 	
-	exports["qb-target"]:AddBoxZone("cokebreak", vector3(Config.Zones.CokeBreak.coords["x"], Config.Zones.CokeBreak.coords["y"], Config.Zones.CokeBreak.coords["z"]), Config.Zones.CokeBreak.width, Config.Zones.CokeBreak.length, {
+exports["qb-target"]:AddBoxZone("cokebreak", vector3(Config.Zones.CokeBreak.coords["x"], Config.Zones.CokeBreak.coords["y"], Config.Zones.CokeBreak.coords["z"]), Config.Zones.CokeBreak.width, Config.Zones.CokeBreak.length, {
         name = "cokebreak",
         heading = Config.Zones.CokeBreak.heading,
         debugPoly = false,
         minZ = Config.Zones.CokeBreak.minZ,
 		maxZ = Config.Zones.CokeBreak.maxZ,
-    }, {
+}, {
         options = {
             {
                 type = "client",
@@ -138,13 +138,13 @@ CreateThread(function()
     distance = 1.5
     })
 
-	exports["qb-target"]:AddBoxZone("cookcrack", vector3(Config.Zones.CookCrack.coords["x"], Config.Zones.CookCrack.coords["y"], Config.Zones.CookCrack.coords["z"]), Config.Zones.CookCrack.width, Config.Zones.CookCrack.length, {
+exports["qb-target"]:AddBoxZone("cookcrack", vector3(Config.Zones.CookCrack.coords["x"], Config.Zones.CookCrack.coords["y"], Config.Zones.CookCrack.coords["z"]), Config.Zones.CookCrack.width, Config.Zones.CookCrack.length, {
         name = "cookcrack",
         heading = Config.Zones.CookCrack.heading,
         debugPoly = false,
         minZ = Config.Zones.CookCrack.minZ,
 		maxZ = Config.Zones.CookCrack.maxZ,
-    }, {
+}, {
         options = {
             {
                 type = "client",
@@ -156,13 +156,13 @@ CreateThread(function()
     distance = 1.2
     })
 
-	exports["qb-target"]:AddBoxZone("CokeQBrick", vector3(Config.Zones.CokeQBrick.coords["x"], Config.Zones.CokeQBrick.coords["y"], Config.Zones.CokeQBrick.coords["z"]), Config.Zones.CokeQBrick.width, Config.Zones.CokeQBrick.length, {
+exports["qb-target"]:AddBoxZone("CokeQBrick", vector3(Config.Zones.CokeQBrick.coords["x"], Config.Zones.CokeQBrick.coords["y"], Config.Zones.CokeQBrick.coords["z"]), Config.Zones.CokeQBrick.width, Config.Zones.CokeQBrick.length, {
         name = "cokeqbrick",
         heading = Config.Zones.CokeQBrick.heading,
         debugPoly = false,
         minZ = Config.Zones.CokeQBrick.minZ,
 		maxZ = Config.Zones.CokeQBrick.maxZ,
-    }, {
+}, {
         options = {
             {
                 type = "client",
@@ -174,13 +174,13 @@ CreateThread(function()
     distance = 1.0
     })
 
-	exports["qb-target"]:AddBoxZone("BreakCokeQBrick", vector3(Config.Zones.CokeQBreak.coords["x"], Config.Zones.CokeQBreak.coords["y"], Config.Zones.CokeQBreak.coords["z"]), Config.Zones.CokeQBreak.width, Config.Zones.CokeQBreak.length, {
+exports["qb-target"]:AddBoxZone("BreakCokeQBrick", vector3(Config.Zones.CokeQBreak.coords["x"], Config.Zones.CokeQBreak.coords["y"], Config.Zones.CokeQBreak.coords["z"]), Config.Zones.CokeQBreak.width, Config.Zones.CokeQBreak.length, {
         name = "cokeqbreak",
         heading = Config.Zones.CokeQBreak.heading,
         debugPoly = false,
         minZ = Config.Zones.CokeQBreak.minZ,
 		maxZ = Config.Zones.CokeQBreak.maxZ,
-    }, {
+}, {
         options = {
             {
                 type = "client",
