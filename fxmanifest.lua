58849aa7f5a58e9a9e6db4ab3d38b9ca6464b2fe
coma <146189420+comaFX@users.noreply.github.com>
2023-11-17 05:07:34 -0600

    Update fxmanifest.lua

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -4,7 +4,7 @@ lua54 'yes'
 
 shared_scripts {
 	'@PolyZone/client.lua',
-    '@PolyZone/CircleZone.lua',
+	'@PolyZone/CircleZone.lua',
 	'@qb-core/shared/locale.lua',
 	'config.lua',
 	'locales/en.lua'
@@ -12,11 +12,33 @@ shared_scripts {
 
 server_scripts {
 	'server/coke.lua',
-
+	'server/heroin.lua',
+	'server/mdma.lua',
+	'server/meth.lua',
+	'server/weed.lua',
+	'server/consumables.lua',
 }
 
 client_scripts {
 	'client/coke.lua',
-	'client/sulfuricacid.lua',
+	'client/heroin.lua',
+	'client/mdma.lua',
+	'client/meth.lua',
+	'client/weed.lua',
 	'client/target.lua',
+	'client/consumables.lua',
+	
 }
+
+--[[
+this_is_a_map 'yes'
+data_file 'DLC_ITYP_REQUEST' 'labs.ytyp'
+
+file 'stream/**.ytyp'
+data_file 'DLC_ITYP_REQUEST' 'stream/**.ytyp'
+--]]
+files {
+	'stream/mw_props.ytyp'
+}
+
+data_file 'DLC_ITYP_REQUEST' 'stream/mw_props.ytyp'
