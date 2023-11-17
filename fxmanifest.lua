45c12953515eb00d48682908e5b985049118d3e5
coma <146189420+comaFX@users.noreply.github.com>
2023-11-17 05:58:00 -0600

    Update fxmanifest.lua

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -1,39 +1,22 @@
 fx_version 'cerulean'
 games { 'gta5' }
 lua54 'yes'
-
+---
 shared_scripts {
 	'@PolyZone/client.lua',
-	'@PolyZone/CircleZone.lua',
+    '@PolyZone/CircleZone.lua',
 	'@qb-core/shared/locale.lua',
 	'config.lua',
 	'locales/en.lua'
 }
-
+---
 server_scripts {
 	'server/coke.lua',
-	'server/heroin.lua',
-	'server/mdma.lua',
-	'server/meth.lua',
-	'server/weed.lua',
-	'server/consumables.lua',
-}
 
+}
+---
 client_scripts {
 	'client/coke.lua',
-	'client/heroin.lua',
-	'client/mdma.lua',
-	'client/meth.lua',
-	'client/weed.lua',
+	'client/sulfuricacid.lua',
 	'client/target.lua',
-	'client/consumables.lua',
-	
 }
-
---[[
-this_is_a_map 'yes'
-data_file 'DLC_ITYP_REQUEST' 'labs.ytyp'
-
-file 'stream/**.ytyp'
-data_file 'DLC_ITYP_REQUEST' 'stream/**.ytyp'
---]]
