ab0fc83fb2dbe13ad3228cf80a0dc522880e695d
coma <146189420+comaFX@users.noreply.github.com>
2023-11-17 23:29:19 -0600

    Update fxmanifest.lua

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -1,20 +1,20 @@
 fx_version 'cerulean'
 games { 'gta5' }
 lua54 'yes'
----
+
 shared_scripts {
 	'@PolyZone/client.lua',
-    '@PolyZone/CircleZone.lua',
+   	'@PolyZone/CircleZone.lua',
 	'@qb-core/shared/locale.lua',
 	'config.lua',
 	'locales/en.lua'
 }
----
+
 server_scripts {
 	'server/coke.lua',
 
 }
----
+
 client_scripts {
 	'client/coke.lua',
 	'client/sulfuricacid.lua',
